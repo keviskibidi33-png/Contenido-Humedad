a520efa5fe7e040321306321bde0205274ddc1e7
Xangel0s <tu-email@ejemplo.com>
2026-03-14 10:50:34 -0500

    Center compact assay fields

--- src/pages/ContHumedadForm.tsx
+++ src/pages/ContHumedadForm.tsx
@@ -286,10 +286,10 @@ export default function ContHumedadForm() {
               </thead>
               <tbody>
                 <tr>
-                  <td className="border-r border-t border-slate-300 p-1"><input className={inputClass} value={form.muestra} onChange={(e) => setField('muestra', e.target.value)} onBlur={() => setField('muestra', normalizeMuestra(form.muestra))} autoComplete="off" data-lpignore="true" /></td>
-                  <td className="border-r border-t border-slate-300 p-1"><input className={inputClass} value={form.numero_ot} onChange={(e) => setField('numero_ot', e.target.value)} onBlur={() => setField('numero_ot', normalizeOt(form.numero_ot))} autoComplete="off" data-lpignore="true" /></td>
-                  <td className="border-r border-t border-slate-300 p-1"><input className={inputClass} value={form.fecha_ensayo} onChange={(e) => setField('fecha_ensayo', e.target.value)} onBlur={() => setField('fecha_ensayo', normalizeDate(form.fecha_ensayo))} autoComplete="off" data-lpignore="true" placeholder="DD/MM/AA" /></td>
-                  <td className="border-t border-slate-300 p-1"><input className={inputClass} value={form.realizado_por} onChange={(e) => setField('realizado_por', e.target.value)} autoComplete="off" data-lpignore="true" /></td>
+                  <td className="border-r border-t border-slate-300 p-1"><input className={`${inputClass} text-center`} value={form.muestra} onChange={(e) => setField('muestra', e.target.value)} onBlur={() => setField('muestra', normalizeMuestra(form.muestra))} autoComplete="off" data-lpignore="true" /></td>
+                  <td className="border-r border-t border-slate-300 p-1"><input className={`${inputClass} text-center`} value={form.numero_ot} onChange={(e) => setField('numero_ot', e.target.value)} onBlur={() => setField('numero_ot', normalizeOt(form.numero_ot))} autoComplete="off" data-lpignore="true" /></td>
+                  <td className="border-r border-t border-slate-300 p-1"><input className={`${inputClass} text-center`} value={form.fecha_ensayo} onChange={(e) => setField('fecha_ensayo', e.target.value)} onBlur={() => setField('fecha_ensayo', normalizeDate(form.fecha_ensayo))} autoComplete="off" data-lpignore="true" placeholder="DD/MM/AA" /></td>
+                  <td className="border-t border-slate-300 p-1"><input className={`${inputClass} text-center`} value={form.realizado_por} onChange={(e) => setField('realizado_por', e.target.value)} autoComplete="off" data-lpignore="true" /></td>
                 </tr>
               </tbody>
             </table>
